322f68214835e53280298167aa9db0087b5ad3a1
domens028 <44053294+domens028@users.noreply.github.com>
2022-06-09 23:05:38 +0200

    authentication + OAuth
    Faltaria hacer el tema de notificaciones y panel de usuario

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 //Requires
 const  express = require('express');
+const bp = require('body-parser');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const session = require('express-session');
 const path = require('path')
-const app = express();
+const passport = require('passport')
 
+//Initializations 
+const app = express();
+require('./database');
+require('./config/passport')
 
 //settings 
 app.set('port',8080);
@@ -10,11 +18,39 @@ app.set('view engine','ejs')
 app.engine('html',require('ejs').renderFile)
 app.set('views',path.join(__dirname,'Views'))
 
-//routes
-app.use(require('./routes/routes'))
+//Middlewares
+app.use(methodOverride('_method'))
+// parse application/json
+app.use(bp.json())
+app.use(bp.urlencoded({extended: false}));
+app.use(session({
+    secret:'mysecret',
+    resave:'true',
+    saveUninitialized:'true'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+//Global variables
+app.use((req,res,next) => {
+
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    res.locals.loggedin = req.session.loggedin;
+    res.locals.user = req.user || null;
+    next();
+
+})
 
 //static files 
 app.use(express.static(path.join(__dirname, "public")));
+
+//routes
+app.use(require('./routes/index'))
+app.use(require('./routes/users'))
+
 
 //listening server
 app.listen(app.get('port'), () => {
